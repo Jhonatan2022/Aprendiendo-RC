7078ad3edfd546caccc71eb87cc2616bed88f8db
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2023-04-13 16:15:44 +0100

    Using Bootstrap5 And Fonts Awesome

--- project-with-vite/src/App.jsx
+++ project-with-vite/src/App.jsx
@@ -1,6 +1,26 @@
+// ALGUNOS ATAJOS
+// rafce + tab = crea una funcion con un return
+// rafc + tab = crea una funcion sin return
+// rconst + tab = crea una constante
+// rafcp + tab = crea una funcion con un return y un proptypes
+// rafce + tab = crea una funcion con un return y un export default
+// -------------------------------------------------------------------------
+
+
+
+// Importamos la libreria de bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Importamos los iconos de fontawesome
+import {  FaJoomla } from "react-icons/fa";
+
+
+
 // Creamos una funcion con un hola mundo
 function App() {
-  return <h1>Hello World</h1>;
+  return <h1> <FaJoomla className='me-3' />
+    Hello World</h1>;
+
 }
 
 
